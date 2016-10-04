795d26e59c5900ce9bddd2536e95354eb23f77fd
Viktor Brusylovets <v.brusylovets@gmail.com>
2016-10-04 23:05:20 +0200

    local storage and its tests added

---
+++ storage.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	_ "github.com/mattn/go-sqlite3"
+	"database/sql"
+	"fmt"
+)
+
+// Storage interface provides methods to use for other code of app, so it doesn't depend on storage implementation.
+type Storage interface {
+	Put(string, []byte) error
+	Get(string) ([]byte, error)
+	Remove(string) error
+	Next() ([]byte, error)
+}
+
+// SqliteStorage embedded storage.
+type SqliteStorage struct {
+	dbPath string
+}
+
+// Init initialise sqlite storage.
+func (storage *SqliteStorage) Init() error {
+	// Create sqlite db file
+	db, err := sql.Open("sqlite3", storage.dbPath)
+	if err != nil {
+		return fmt.Errorf("Storage: unable to create db. %s", err.Error())
+	}
+	defer db.Close()
+	// Create table if it' doesn't exist
+	query := `
+	CREATE TABLE IF NOT EXISTS jobs(
+		id TEXT NOT NULL PRIMARY KEY,
+		addedAt DATETIME,
+		data BLOB
+	);
+	`
+	_, err = db.Exec(query)
+	if err != nil {
+		return fmt.Errorf("Storage: unable to create jobs table. %s", err.Error())
+	}
+	return nil
+}
+
+// Put saves job to the storage.
+func (storage *SqliteStorage) Put(id string, data []byte) error {
+	db, err := sql.Open("sqlite3", storage.dbPath)
+	if err != nil {
+		return fmt.Errorf("Storage: PUT %s, open db failed. %s", id, err.Error())
+	}
+	defer db.Close()
+
+	tx, err := db.Begin()
+	if err != nil {
+		return fmt.Errorf("Storage: PUT %s, create transaction failed. %s", id, err.Error())
+	}
+	stmt, err := tx.Prepare("INSERT INTO jobs(id, addedAt, data) VALUES(?, datetime('now'), ?)")
+	if err != nil {
+		return fmt.Errorf("Storage: PUT %s, unable to prepare statement. %s", id, err.Error())
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(id, data)
+	if err != nil {
+		return fmt.Errorf("Storage: PUT %s, execute failed. %s", id, err.Error())
+	}
+	tx.Commit()
+
+	return nil
+}
+
+// Get saves job to the storage.
+func (storage *SqliteStorage) Get(id string) ([]byte, error) {
+	db, err := sql.Open("sqlite3", storage.dbPath)
+	if err != nil {
+		return nil, fmt.Errorf("Storage: GET %s, open db failed. %s", id, err.Error())
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("SELECT data FROM jobs WHERE id = ?")
+	if err != nil {
+		return nil, fmt.Errorf("Storage: GET %s, prepare statement failed. %s", id, err.Error())
+	}
+	defer stmt.Close()
+
+	var data []byte
+	err = stmt.QueryRow(id).Scan(&data)
+	if err != nil {
+		return nil, fmt.Errorf("Storage: GET %s, prepare statement failed. %s", id, err.Error())
+	}
+
+	return data, nil
+}
+
+// Remove job from the storage by id
+func (storage *SqliteStorage) Remove(id string) error {
+	return nil
+}
+
+// Next returns any item needs to be processed.
+func (storage *SqliteStorage) Next() ([]byte, error) {
+	return nil, nil
+}
+
+// NewStorage create new storage entity.
+func NewStorage(dbPath string) (Storage, error) {
+
+	if dbPath == "" {
+		return nil, fmt.Errorf("Storage: please provide non-empty path to the storage")
+	}
+
+	storage := SqliteStorage{dbPath: dbPath}
+	err := storage.Init()
+	if err != nil {
+		return nil, err
+	}
+
+	return &storage, nil
+}
+
---
+++ storage_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"testing"
+	"os"
+)
+
+const TestDBName string = "testdata/test.db"
+
+func TestNewStorage(t *testing.T) {
+	os.Remove(TestDBName)
+
+	storage, err := NewStorage(TestDBName)
+	if err != nil {
+		t.Errorf("Unable to create new storage: %s", err.Error())
+	}
+	data := `{"url":"http://google.com"}`
+	err = storage.Put("id1", []byte(data))
+	if err != nil {
+		t.Errorf("Unable to put data to new storage: %s", err.Error())
+	}
+
+	savedData, err := storage.Get("id1")
+	if err != nil {
+		t.Errorf("Unable to put data to new storage: %s", err.Error())
+	}
+
+	savedDataStr := string(savedData)
+
+	if savedDataStr != "" && savedDataStr != data {
+		t.Errorf("Expected %s is not equal to given %s", data, string(savedData))
+	}
+}
